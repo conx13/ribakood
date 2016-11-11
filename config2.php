3dda4369660ebcda59e538d776ba85dc9940aec5
conx <hillar@gmail.com>
2016-11-11 15:56:26 +0200

    näpukad

--- config2.php
+++ config2.php
@@ -1,5 +1,5 @@
 <?php
-	$myServer = "HILLAR_PC\SERVER";
+	$myServer = "10.0.30.103,2100";
 	$connection_info = array( "Database"=>"Uus_kood", "UID"=>"Hillar", "PWD"=>"conx13", "ReturnDatesAsStrings"=> "true", 'CharacterSet'=>'UTF-8'); 
 	$conn = sqlsrv_connect($myServer, $connection_info)
   		or die("Kahjuks ei saa serveriga ühendust"); 
